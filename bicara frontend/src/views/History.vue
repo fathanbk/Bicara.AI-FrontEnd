d80a0f522604982621b4e3aff450a9bb82bc2999
salwatsa <salwa130703@gmail.com>
2022-12-29 22:57:50 +0700

    Added empty state for "History"

--- bicara frontend/src/views/History.vue
+++ bicara frontend/src/views/History.vue
@@ -5,17 +5,27 @@
         <button slot="start" v-on:click="open_side()" id="menu">
           <span class="material-symbols-rounded menu"> menu </span>
         </button>
-        <ion-title>Bicara.ai</ion-title>
+        <ion-img src="assets/img/bicara-logo.svg" style="height: 70px" slot="start"></ion-img>
         <ion-button id="open-modal-upload" slot="end">
           <span class="material-symbols-outlined"> video_call </span>
           <p class="upload-nav" style="margin-left: 10px">Upload Video</p>
         </ion-button>
-        <ion-button slot="end">
-          <span class="material-symbols-outlined"> search </span>
-        </ion-button>
-        <ion-avatar slot="end" style="width: 38px; height: 38px; margin-left: 10px">
-          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-        </ion-avatar>
+        <a class="avatar" slot="end" id="click-trigger">
+          <ion-avatar slot="end" style="width: 38px; height: 38px; margin-left: 10px">
+            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+          </ion-avatar>
+        </a>
+        <ion-popover trigger="click-trigger" trigger-action="click">
+          <ion-content class="ion-padding">
+            <ion-list>
+              <ion-avatar slot="end" style="width: 45px; height: 45px">
+                <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+              </ion-avatar>
+              <p class="nameacc">Jake Doe</p>
+              <p class="emailacc">jakedoe@hotemail.com</p>
+            </ion-list>
+          </ion-content>
+        </ion-popover>
 
         <ion-modal id="example-modal" ref="modal" trigger="open-modal-upload">
           <div class="wrapper" @dragover="dragover" @dragleave="dragleave" @drop="drop">
@@ -37,6 +47,14 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
+      <!--Mobile -->
+      <div class="background-mobile"></div>
+      <div class="intro-mobile-2">
+        <h1 slot="start" style="margin: 10px 3px">Bicara.ai</h1>
+        <button slot="end" v-on:click="open_side()" id="menu">
+          <span class="material-symbols-rounded menu"> menu </span>
+        </button>
+      </div>
       <div class="dashboard">
         <!-- Sidebar History-->
         <div class="aside w3-animate-left" id="aside" style="position: fixed">
@@ -56,9 +74,17 @@
               <span class="material-symbols-outlined"> video_library </span>
               <p>History</p>
             </a>
+            <a href="/library">
+              <span class="material-symbols-outlined"> video_library</span>
+              <p>Library</p>
+            </a>
             <a href="#">
               <span class="material-symbols-outlined"> help_center </span>
               <p>FAQ</p>
+            </a>
+            <a class="onlymobile" id="open-modal-upload">
+              <span class="material-symbols-outlined"> video_call </span>
+              <p>Upload Video</p>
             </a>
           </div>
           <div class="lastsidebar">
@@ -75,23 +101,32 @@
 
         <!-- Content History -->
         <div class="content">
-          <ion-card class="history-video">
+          <h1>Jake's History</h1>
+          <div class="emptystatehistory" style="display: none">
+            <ion-card class="bghistory">
+              <ion-img src="assets/icon/history-vid.svg"></ion-img>
+            </ion-card>
+            <h2>Discover powerful analytics by recording a speech,</h2>
+            <h2>get your speech feedback now.</h2>
+            <a href="#"><h2 style="text-decoration-line: underline; color: #3f54d1; text-align: center;">Upload your video</h2></a>
+          </div>
+          <ion-card class="history-video" >
             <ion-grid>
-              <ion-row >
+              <ion-row>
                 <ion-col>
-                  <h4>Video</h4>
+                  <h4 class="title">Video</h4>
                 </ion-col>
                 <ion-col>
-                  <h4>Upload</h4>
+                  <h4 class="title">Upload</h4>
                 </ion-col>
                 <ion-col>
-                  <h4>Duration</h4>
+                  <h4 class="title">Duration</h4>
                 </ion-col>
                 <ion-col>
-                  <h4>Pacing (word/min)</h4>
+                  <h4 class="title">Pacing (word/min)</h4>
                 </ion-col>
                 <ion-col>
-                  <h4>Filler Word</h4>
+                  <h4 class="title">Filler Word</h4>
                 </ion-col>
               </ion-row>
               <ion-row class="table-history">
@@ -111,7 +146,6 @@
                   <p class="tablecontent">4 Filler</p>
                 </ion-col>
               </ion-row>
-
             </ion-grid>
           </ion-card>
         </div>
@@ -121,7 +155,7 @@
 </template>
 
 <script lang="ts">
-import { IonButton, IonContent, IonPage, IonHeader, IonTitle, IonToolbar, IonModal } from "@ionic/vue";
+import { IonButton, IonContent, IonPage, IonHeader, IonToolbar, IonModal } from "@ionic/vue";
 import axios from "axios";
 import { defineComponent, ref } from "vue";
 
@@ -132,14 +166,13 @@ export default defineComponent({
     IonContent,
     IonPage,
     IonHeader,
-    IonTitle,
     IonToolbar,
     IonModal,
   },
   data() {
     return {
       isDragging: false,
-      file: '',
+      file: "",
     };
   },
   methods: {
@@ -153,7 +186,7 @@ export default defineComponent({
       (document.getElementById("aside") as HTMLInputElement).style.display = "block";
       (document.getElementById("close") as HTMLInputElement).style.display = "inline-block";
     },
-    onChange(e : { target: { files: any; }; }) {
+    onChange(e: { target: { files: any } }) {
       console.log(e.target.files[0]);
       this.file = e.target.files[0];
       (document.getElementById("upload_button") as HTMLInputElement).disabled = false;
@@ -171,24 +204,22 @@ export default defineComponent({
       console.log(this.file);
       (document.getElementById("upload_button") as HTMLInputElement).disabled = false;
       this.isDragging = false;
-      
     },
     upload_video() {
       let formData = new FormData();
       formData.append("file", this.file);
-      axios.post("http://127.0.0.1:5000/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        
-      }).then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      }
-      
-      );
+      axios
+        .post("http://127.0.0.1:5000/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 });
@@ -218,6 +249,10 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+ion-header ion-img {
+  margin-top: -10px;
 }
 
 ion-page {
@@ -281,6 +316,17 @@ ion-header ion-toolbar button .close {
 ion-button i {
   width: 10px;
   margin-right: 15px;
+}
+
+.avatar {
+  width: fit-content;
+  height: fit-content;
+}
+
+ion-popover {
+  --backdrop-opacity: 0;
+  margin-top: 10px;
+  margin-right: 10px;
 }
 
 ion-modal#example-modal {
@@ -368,6 +414,33 @@ ion-modal#example-modal {
 #close {
   background-color: #ffffff;
   color: black;
+}
+
+ion-popover {
+  font-family: "Segoe UI", Arial, sans-serif;
+  font-weight: 600;
+  font-size: 18px;
+  border-radius: 10px !important;
+}
+
+ion-list,
+ion-item {
+  background-color: #ffffff;
+  padding: 0px;
+}
+
+.nameacc,
+.emailacc {
+  margin: 5px 0px;
+}
+
+.nameacc {
+  margin-top: 15px;
+}
+
+.emailacc {
+  color: #8c8c8c;
+  font-size: 16px;
 }
 
 /* CSS SIDEBAR */
@@ -477,7 +550,7 @@ ion-modal#example-modal {
 }
 .aside .lastsidebar {
   position: absolute;
-  bottom: 80px;
+  bottom: 90px;
   border-top: 1px solid #8c8c8c;
   padding: 10px 25px;
   width: 100%;
@@ -513,8 +586,7 @@ ion-modal#example-modal {
 
 .content h1 {
   font-size: 30px;
-  margin-left: 8px;
-
+  margin: 20px 8px;
 }
 
 ion-card {
@@ -526,26 +598,72 @@ ion-card {
   padding: 0 !important;
 }
 
-h4, .tablecontent{
+.title,
+.tablecontent {
   text-align: center;
   font-family: "Segoe UI", Arial, sans-serif;
 }
 
-.tablecontent{
+.tablecontent {
   font-size: 18px;
   font-weight: 450;
 }
 
-ion-grid{
+ion-grid {
   padding: 0;
 }
-.table-history{
+.table-history {
   border-top: 1px solid #8c8c8c;
   height: 120px;
 }
 
+.bghistory {
+  max-width: 450px;
+  min-height: 200px;
+  margin: 0px auto;
+}
+
+.bghistory > ion-img {
+  width: 200px;
+  margin: 50px auto;
+}
+
+.emptystatehistory > h2 {
+  text-align: center;
+}
+
+@media (max-width: 992px) {
+  .content {
+    width: 576px;
+  }
+
+  .upload-nav {
+    display: none;
+  }
+}
 
 @media (max-width: 576px) {
+
+  .intro-mobile-2 {
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #3f54d1;
+    padding: 10px 20px;
+    padding-bottom: 0px;
+    width: 100%;
+    margin: 0px;
+    z-index: 200;
+  }
+  .intro-mobile-2 h1 {
+    margin-top: 2px;
+    color: #ffffff;
+    font-family: "Krona One", sans-serif;
+    font-weight: 400;
+    font-size: 24px;
+  }
+
   .lastsidebar {
     bottom: 3px !important;
   }
@@ -569,6 +687,11 @@ ion-grid{
 
   .content h1 {
     font-size: 25px;
+    margin-top: 80px;
+  }
+  .content h2 {
+    font-size: 5vw;
+    line-height: normal;
   }
 
   .content .title-rat {
