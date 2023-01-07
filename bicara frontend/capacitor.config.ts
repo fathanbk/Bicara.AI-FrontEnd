b239777a0ff80635ba7b186bd0075e5b66e6f514
Aldy Syachranie <aldysyach17@gmail.com>
2023-01-07 22:17:11 +0700

    Update appname and appid in config and package json

--- bicara frontend/capacitor.config.ts
+++ bicara frontend/capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'bicara.ai',
-  appName: 'bicara.ai',
-  webDir: 'dist',
-  bundledWebRuntime: false
+    appId: "bicaraai",
+    appName: "bicaraai",
+    webDir: "dist",
+    bundledWebRuntime: false,
 };
 
 export default config;
